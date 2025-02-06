565401609cb63e9fc5323bcc8b5624e23e1cbd4b
T3Marius <168598779+T3Marius@users.noreply.github.com>
2025-02-07 01:30:55 +0200

    Add files via upload

---
+++ IJailAPI.cs
@@ -0,0 +1,22 @@
+﻿using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Capabilities;
+
+namespace JailAPI;
+
+public interface IJailAPI
+{
+    public static PluginCapability<IJailAPI?> PluginCapabilty { get; } = new("jailcore:core");
+
+    public void SetSimon(int slot);
+    public bool IsSimon(CCSPlayerController player);
+    public void RemoveSimon();
+    public void RemoveIfSimon(CCSPlayerController? player);
+    public void RemoveSimonInterval();
+    public CCSPlayerController? GetSimon();
+
+    public void SetDeputy(int slot);
+    public bool IsDeputy(CCSPlayerController? player);
+    public void RemoveDeputy();
+    public void RemoveIfDeputy(CCSPlayerController? player);
+    public CCSPlayerController? GetDeputy();
+}
